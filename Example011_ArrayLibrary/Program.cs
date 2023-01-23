b04a1730d5cf3eae8b0f00fb9722ce2ace1c6d2a
VladimirK <vladimir1kuzmin@gmail.com>
2023-01-23 14:58:35 +0700

    Example 011 ArrayLibrary

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -19,9 +19,32 @@ int position = 0;
         position++;
     }
 }
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
 int[] array = new int[10];
 
 FillArray(array);
+array[4] = 4;
+array[6] = 4;
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
 
 
